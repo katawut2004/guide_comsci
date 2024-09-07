a063e100096b16559c229c926b66a47c8ce2f05b
Mr.katawut <mr.katawut2004@gmail.com>
2024-09-08 01:02:18 +0700

    update settings

--- webpage/views.py
+++ webpage/views.py
@@ -1,26 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def booking(request):
-    return render(request, 'booking.html')
-
-def contact(request):
-    return render(request, 'contact.html')
+def service(request):
+    return render(request, 'service.html')
 
 def menu(request):
     return render(request, 'menu.html')
 
-def service(request):
-    return render(request, 'service.html')
+def booking(request):
+    return render(request, 'booking.html')
 
 def team(request):
     return render(request, 'team.html')
 
-def testimonial(request):
+def testimonialni(request):
     return render(request, 'testimonial.html')
+
+def contact(request):
+    return render(request, 'contact.html')
