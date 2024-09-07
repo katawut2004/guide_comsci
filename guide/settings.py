ac1de6b47713438ec6b3e26fb253b8d40cedb25f
Mr.katawut <mr.katawut2004@gmail.com>
2024-09-08 00:57:42 +0700

    update settings

--- guide/settings.py
+++ guide/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "assets"
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-    
-]
+
+STATICFILES_DIRS = [BASE_DIR / 'statics', ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
