4d6db9c8de391551d80462d675e6f3fc3a84ba12
Mr.katawut <mr.katawut2004@gmail.com>
2024-09-08 01:37:51 +0700

    sh

--- guide/settings.py
+++ guide/settings.py
@@ -125,7 +125,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Whitenoise settings (optional)
