ed64d0534fa08a4f61e4b4cc8def0766c76a3ba2
Mr.katawut <mr.katawut2004@gmail.com>
2024-09-08 00:34:42 +0700

    frist_commit

---
+++ webpage/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from webpage.views import about, service, index, menu, booking, team, testimonial, contact
+
+
+urlpatterns = [
+    path("", index),
+    path("about/", about, name='about'),
+    path("service/", service, name='service'),
+    path("menu/", menu, name='menu'),
+    path("booking/", booking, name='booking'),
+    path("our team/", team, name='team'),
+    path("testimonial/", testimonial, name='testimonial'),
+    path("contact/", contact, name='contact'), 
+    
+]
