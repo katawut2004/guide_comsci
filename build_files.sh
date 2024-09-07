bfb47a48252f97c56f50a67a48bd584d291ca199
Mr.katawut <mr.katawut2004@gmail.com>
2024-09-08 01:28:25 +0700

    set build_files

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
+#!/bin/bash
+
+# Collect static files
+echo "Collect static files"
+python3.9 manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python3.9 manage.py migrate
