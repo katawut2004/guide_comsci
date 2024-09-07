51afb90945a2f434f0740c28f7d719632e532f62
Mr.katawut <mr.katawut2004@gmail.com>
2024-09-08 00:50:49 +0700

    update url.py

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
-
 from webpage.views import about, service, index, menu, booking, team, testimonial, contact
 
-
 urlpatterns = [
-    path("", index),
+    path("", index, name='index'),
     path("about/", about, name='about'),
     path("service/", service, name='service'),
     path("menu/", menu, name='menu'),
     path("booking/", booking, name='booking'),
     path("our team/", team, name='team'),
     path("testimonial/", testimonial, name='testimonial'),
-    path("contact/", contact, name='contact'), 
-    
+    path("contact/", contact, name='contact'),
 ]
